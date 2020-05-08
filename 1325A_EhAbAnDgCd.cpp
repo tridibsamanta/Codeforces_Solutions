4b3837c8ba69c7c812b5e1c7061133e124f5e3fb
Tridib Samanta <tridibsamanta2010@gmail.com>
2020-05-08 22:51:54 +0530

    1325A. EhAb AnD gCd

---
+++ 1325A_EhAbAnDgCd.cpp
@@ -0,0 +1,27 @@
+/*
+~ Author    : @tridib_2003
+~ Title     : 1325A. EhAb AnD gCd
+~ Link      : https://codeforces.com/contest/1325/problem/A
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int tc;
+	cin >> tc;
+	while (tc--) {
+
+		int x;
+		cin >> x;
+
+		cout << "1 " << x - 1 << '\n';
+
+	}
+
+	return 0;
+}
