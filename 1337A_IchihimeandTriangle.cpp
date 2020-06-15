e120cbcfb1d449061c75358c4aa0e66977553523
Tridib Samanta <tridibsamanta2010@gmail.com>
2020-06-15 23:50:17 +0530

    1337A. Ichihime and Triangle

---
+++ 1337A_IchihimeandTriangle.cpp
@@ -0,0 +1,30 @@
+/*
+~ Author    : @tridib_2003
+~ Title     : 1337A. Ichihime and Triangle
+~ Link      : https://codeforces.com/contest/1337/problem/A
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tc;
+    cin >> tc;
+    while (tc--) {
+
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+
+        int x = b, y = c, z = c;
+
+        cout << x << " " << y << " " << z << '\n';
+
+    }
+
+    return 0;
+}
