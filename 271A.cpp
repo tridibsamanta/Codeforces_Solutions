ed3184ef049bfe692a161ffdc03f2f536a81c228
Tridib Samanta <tridibsamanta2010@gmail.com>
2020-11-23 23:57:27 +0530

    271A. Beautiful Year

---
+++ 271A.cpp
@@ -0,0 +1,48 @@
+/*
+ ~ Author    : @tridib_2003
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll                  long long
+#define PI                  3.1415926535897932384626
+#define MOD                 1000000007
+#define vi                  vector<int>
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(a, c)           for (int(a) = 0; (a) < (c); (a)++)
+#define w(x)                int x; cin >> x; while(x--)
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+int main() {
+
+	FIO;
+
+	int n;
+	cin >> n;
+
+	unordered_set<int> unique;
+
+	while (unique.size() != 4) {
+
+		unique.clear();
+
+		int temp = ++n;
+
+		unique.insert(temp % 10);
+		temp /= 10;
+
+		unique.insert(temp % 10);
+		temp /= 10;
+
+		unique.insert(temp % 10);
+		temp /= 10;
+
+		unique.insert(temp);
+
+	}
+
+	cout << n << '\n';
+
+	return 0;
+}
