543a65c0abd74ff575c63937fb4622532a385bb3
Tridib Samanta <tridibsamanta2010@gmail.com>
2020-11-28 23:54:10 +0530

    110B. Lucky String

---
+++ 110B.cpp
@@ -0,0 +1,41 @@
+/*
+ ~ Author    : @tridib_2003
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll                  long long
+#define PI                  3.1415926535897932384626
+#define MOD                 1000000007
+#define vi                  vector<int>
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(a, c)           for (int(a) = 0; (a) < (c); (a)++)
+#define w(x)                int x; cin >> x; while(x--)
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+int main() {
+
+	FIO;
+
+	int n;
+	cin >> n;
+
+	string pattern = "abcd", res = "";
+
+	if (n < 5) {
+		cout << pattern.substr(0, n);
+	}
+	else {
+
+		int repeat = n / 4;
+		while (repeat--)
+			res.append(pattern);
+
+		int rem = n % 4;
+		res.append(pattern.substr(0, rem));
+		cout << res << '\n';
+	}
+
+	return 0;
+}
