f5e82c1fe55c201e5c62d8f9c75cce0440b738fd
Tridib Samanta <tridibsamanta2010@gmail.com>
2020-12-12 23:59:08 +0530

    231A. Team

---
+++ 231A.cpp
@@ -0,0 +1,38 @@
+/*
+ ~ Author    : @tridib_2003
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll                  long long
+#define PI                  3.1415926535897932384626
+#define MOD                 1000000007
+#define vi                  vector<int>
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(a, c)           for (int(a) = 0; (a) < (c); (a)++)
+#define w(x)                int x; cin >> x; while(x--)
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+int main() {
+
+	FIO;
+
+	int n;
+	cin >> n;
+
+	int cnt = 0;
+
+	FOR(i, n) {
+
+		int p, v, t;
+		cin >> p >> v >> t;
+
+		if ((p == 1 && v == 1) || (p == 1 && t == 1) || (v == 1 && t == 1))
+			++cnt;
+	}
+
+	cout << cnt;
+
+	return 0;
+}
