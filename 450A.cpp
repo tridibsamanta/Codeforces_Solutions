4dff9f5907fdd3a1d7610bd541935b28e3778383
Tridib Samanta <tridibsamanta2010@gmail.com>
2020-12-13 16:35:01 +0530

    450A. Jzzhu and Children

---
+++ 450A.cpp
@@ -0,0 +1,39 @@
+/*
+ ~ Author    : @tridib_2003
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll                  long long
+#define PI                  3.1415926535897932384626
+#define MOD                 1000000007
+#define vi                  vector<int>
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(a, c)           for (int(a) = 0; (a) < (c); (a)++)
+#define w(x)                int x; cin >> x; while(x--)
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+int main() {
+
+	FIO;
+
+	double n, m;
+	cin >> n >> m;
+
+	double last_idx = 0, last_turn = 0;
+
+	FOR(i, n) {
+		double a;
+		cin >> a;
+
+		if (ceil(a / m) >= last_turn) {
+			last_turn = ceil(a / m);
+			last_idx = i + 1;
+		}
+	}
+
+	cout << last_idx;
+
+	return 0;
+}
