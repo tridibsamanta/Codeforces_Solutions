b9eef697eedcb973bd85ae006e64210fae5a2fbc
Tridib Samanta <tridibsamanta2010@gmail.com>
2020-12-15 23:58:36 +0530

    1462A. Favorite Sequence

---
+++ 1462A.cpp
@@ -0,0 +1,47 @@
+/*
+ ~ Author    : @tridib_2003
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll                  long long
+#define PI                  3.1415926535897932384626
+#define MOD                 1000000007
+#define vi                  vector<int>
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(a, c)           for (int(a) = 0; (a) < (c); (a)++)
+#define w(x)                int x; cin >> x; while(x--)
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+int main() {
+
+	FIO;
+
+	w (tc) {
+
+		int n;
+		cin >> n;
+
+		vector<int> nums(n);
+		FOR(i, n) {
+			cin >> nums[i];
+		}
+
+		int l = 0, r = n - 1;
+
+		while (l <= r) {
+			if (l == r)
+				cout << nums[l] << ' ';
+			else
+				cout << nums[l] << ' ' << nums[r] << ' ';
+			++l;
+			--r;
+		}
+
+		cout << '\n';
+
+	}
+
+	return 0;
+}
