58b13e70876bdffdb7fe8f10db4df5f99e23d1e7
Tridib Samanta <tridibsamanta2010@gmail.com>
2020-12-25 23:58:30 +0530

    221A. Little Elephant and Function

---
+++ 221A.cpp
@@ -0,0 +1,41 @@
+/*
+ ~ Author    : @tridib_2003
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll                  long long
+#define PI                  3.1415926535897932384626
+#define MOD                 1000000007
+#define vi                  vector<int>
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(a, c)           for (int(a) = 0; (a) < (c); (a)++)
+#define w(x)                int x; cin >> x; while(x--)
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+void solve() {
+
+	int n;
+	cin >> n;
+
+	cout << n << ' ';
+
+	for (int i = 1; i < n; ++i)
+		cout << i << ' ';
+}
+
+int main() {
+
+	FIO;
+
+	int tc = 1;
+	// cin >> tc;
+	while (tc--) {
+
+		solve();
+		// cout << '\n';
+	}
+
+	return 0;
+}
