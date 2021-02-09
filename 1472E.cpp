e69cb9fd110818cb9e3171cd801b5054c90f29d5
Tridib Samanta <tridibsamanta2010@gmail.com>
2021-02-09 23:19:47 +0530

    1472E. Correct Placement

---
+++ 1472E.cpp
@@ -0,0 +1,87 @@
+/*
+ ~ Author    : @tridib_2003
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll                  long long
+#define ull                 unsigned long long
+#define PI                  3.1415926535897932384626
+#define MOD                 1000000007
+#define vi                  vector<int>
+#define vvi                 vector<vector<int> >
+#define pb                  push_back
+#define eb                  emplace_back
+#define mp(a, b)            make_pair(a, b)
+#define pii                 pair<int, int>
+#define vpii                vector<pair<int, int> >
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(i, a, b)        for (int(i) = (a); (i) < (b); ++(i))
+#define RFOR(i, a, b)       for (int(i) = (a)-1; (i) >= (b); --(i))
+#define w(x)                int x; cin >> x; while(x--)
+#define all(c)              (c).begin(), (c).end()
+#define sz(c)               (int)(c).size()
+#define pqmx                priority_queue<int>
+#define pqmn                priority_queue<int, vector<int>, greater<int> >
+#define mx_all(c)           *max_element((c).begin(), (c).end())
+#define mn_all(c)           *min_element((c).begin(), (c).end())
+#define lwr_b(c, a)         lower_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define upr_b(c, a)         upper_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+void solve() {
+
+	int n;
+	cin >> n;
+
+	vvi dims;
+
+	FOR (i, 0, n) {
+		int h, w;
+		cin >> h >> w;
+
+		dims.eb(vi {h, -w, i});
+		dims.eb(vi {w, -h, i});
+	}
+
+	sort(all(dims));
+
+	int minWidth = INT_MAX, pos = -1;
+
+	vector<int> ans(n, -2);
+
+	for (int i = 0; i < dims.size(); ++i) {
+		int h = dims[i][0];
+		int w = -dims[i][1];
+		int idx = dims[i][2];
+
+		if (w > minWidth) {
+			ans[idx] = pos;
+		}
+		else if (w < minWidth) {
+			minWidth = w;
+			pos = idx;
+		}
+	}
+
+	FOR (i, 0, n) {
+		cout << ans[i] + 1 << ' ';
+	}
+
+}
+
+int main() {
+
+	FIO;
+
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		solve();
+		cout << '\n';
+	}
+
+	return 0;
+}
