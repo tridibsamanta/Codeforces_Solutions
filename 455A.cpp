c534a3dc1532cd6abb507d9fdf3aca33701b439c
Tridib Samanta <tridibsamanta2010@gmail.com>
2021-03-26 03:51:37 +0530

    455A. Boredom

---
+++ 455A.cpp
@@ -0,0 +1,72 @@
+/*
+ ~ Author    : @tridib_2003
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll                  long long
+#define ull                 unsigned long long
+#define PI                  3.1415926535897932384626
+#define MOD                 1000000007
+#define vi                  vector<int>
+#define vvi                 vector<vector<int> >
+#define pb                  push_back
+#define eb                  emplace_back
+#define mp(a, b)            make_pair(a, b)
+#define pii                 pair<int, int>
+#define vpii                vector<pair<int, int> >
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(i, a, b)        for (int(i) = (a); (i) < (b); ++(i))
+#define RFOR(i, a, b)       for (int(i) = (a)-1; (i) >= (b); --(i))
+#define w(x)                int x; cin >> x; while(x--)
+#define all(c)              (c).begin(), (c).end()
+#define sz(c)               (int)(c).size()
+#define pqmx                priority_queue<int>
+#define pqmn                priority_queue<int, vector<int>, greater<int> >
+#define mx_all(c)           *max_element((c).begin(), (c).end())
+#define mn_all(c)           *min_element((c).begin(), (c).end())
+#define lwr_b(c, a)         lower_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define upr_b(c, a)         upper_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+ll freq[100001];
+ll dp[100001];
+
+void solve() {
+
+	int n;
+	cin >> n;
+
+	FOR (i, 0, n) {
+		int val;
+		cin >> val;
+
+		freq[val]++;
+	}
+
+	dp[0] = 0;
+	dp[1] = freq[1];
+
+	for (int i = 2; i <= 100001; i++) {
+		dp[i] = max(dp[i - 1], dp[i - 2] + (freq[i] * i));
+	}
+
+	cout << dp[100000];
+}
+
+
+int main() {
+
+	FIO;
+
+	int t = 1;
+	// cin >> t;
+	while (t--) {
+		solve();
+		cout << '\n';
+	}
+
+	return 0;
+}
