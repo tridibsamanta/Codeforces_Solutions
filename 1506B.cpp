e1e24e0add861d040e64713bbe52675dfb8c6def
Tridib Samanta <tridibsamanta2010@gmail.com>
2021-03-30 02:22:29 +0530

    1506B. Partial Replacement

---
+++ 1506B.cpp
@@ -0,0 +1,89 @@
+/*
+ ~ Author    : @tridib_2003
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll                  long long
+#define ull                 unsigned long long
+#define PI                  3.1415926535897932384626
+#define MOD                 1000000007
+#define vi                  vector<int>
+#define vvi                 vector<vector<int> >
+#define pb                  push_back
+#define eb                  emplace_back
+#define mp(a, b)            make_pair(a, b)
+#define pii                 pair<int, int>
+#define vpii                vector<pair<int, int> >
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(i, a, b)        for (int(i) = (a); (i) < (b); ++(i))
+#define RFOR(i, a, b)       for (int(i) = (a)-1; (i) >= (b); --(i))
+#define w(x)                int x; cin >> x; while(x--)
+#define all(c)              (c).begin(), (c).end()
+#define sz(c)               (int)(c).size()
+#define pqmx                priority_queue<int>
+#define pqmn                priority_queue<int, vector<int>, greater<int> >
+#define mx_all(c)           *max_element((c).begin(), (c).end())
+#define mn_all(c)           *min_element((c).begin(), (c).end())
+#define lwr_b(c, a)         lower_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define upr_b(c, a)         upper_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+void solve() {
+
+	int n, k;
+	cin >> n >> k;
+
+	string s;
+	cin >> s;
+
+	int cnt = 0, idx = -1;
+
+	FOR (i, 0, n) {
+		if (s[i] == '*') {
+			s[i] = 'x';
+			idx = i;
+			++cnt;
+			break;
+		}
+	}
+
+	while (1) {
+
+		bool flag = false;
+
+		RFOR (i, min(n, idx + k + 1), idx + 1) {
+
+			if (s[i] == '*') {
+				s[i] = 'x';
+				idx = i;
+				++cnt;
+				flag = true;
+				break;
+			}
+		}
+
+		if (!flag)
+			break;
+	}
+
+	// cout << s << '\n';
+	cout << cnt;
+}
+
+
+int main() {
+
+	FIO;
+
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		solve();
+		cout << '\n';
+	}
+
+	return 0;
+}
