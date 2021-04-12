6e031a1085094aca7bc34ded5cd3882a188f0420
Tridib Samanta <tridibsamanta2010@gmail.com>
2021-04-12 17:28:26 +0530

    1512F. Education

---
+++ 1512F.cpp
@@ -0,0 +1,90 @@
+/*
+ ~ Author    : @tridib_2003
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll                  long long
+#define ull                 unsigned long long
+#define PI                  3.1415926535897932384626
+#define MOD                 1000000007
+#define vi                  vector<int>
+#define vvi                 vector<vector<int> >
+#define pb                  push_back
+#define eb                  emplace_back
+#define mp(a, b)            make_pair(a, b)
+#define pii                 pair<int, int>
+#define vpii                vector<pair<int, int> >
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(i, a, b)        for (int(i) = (a); (i) < (b); ++(i))
+#define RFOR(i, a, b)       for (int(i) = (a)-1; (i) >= (b); --(i))
+#define FORALL(a,x)         for (auto& a : x)
+#define w(x)                int x; cin >> x; while(x--)
+#define all(c)              (c).begin(), (c).end()
+#define sz(c)               (int)(c).size()
+#define pqmx                priority_queue<int>
+#define pqmn                priority_queue<int, vector<int>, greater<int> >
+#define max3(a, b, c)       max((a), max((b), (c)))
+#define min3(a, b, c)       min((a), min((b), (c)))
+#define mx_all(c)           *max_element((c).begin(), (c).end())
+#define mn_all(c)           *min_element((c).begin(), (c).end())
+#define lwr_b(c, a)         lower_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define upr_b(c, a)         upper_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+void solve() {
+
+	int n, c;
+	cin >> n >> c;
+
+	vi a(n);
+
+	FOR(i, 0, n) {
+		cin >> a[i];
+	}
+
+	vi b(n - 1);
+
+	FOR (i, 0, n - 1) {
+		cin >> b[i];
+	}
+
+	ll ans = INT_MAX, money = 0, days = 0;
+
+	FOR (i, 0, n) {
+
+		ll wage = a[i];
+
+		ll curr = max(0LL, (c - money + wage - 1) / wage);
+
+		ans = min(ans,  days + curr);
+
+		if (i < n - 1) {
+
+			ll upgrade = max(0LL, (b[i] - money + wage - 1) / wage);
+
+			money += (upgrade * a[i]) - b[i];
+
+			days += 1 + upgrade; // buying the course takes a day
+		}
+
+	}
+
+	cout << ans;
+}
+
+int main() {
+
+	FIO;
+
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		solve();
+		cout << '\n';
+	}
+
+	return 0;
+}
