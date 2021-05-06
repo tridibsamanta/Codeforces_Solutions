db7b7a7d3410c5080cd0f657abf7df35e289e441
Tridib Samanta <tridibsamanta2010@gmail.com>
2021-05-06 13:30:39 +0530

    260A. Adding Digits

---
+++ 260A.cpp
@@ -0,0 +1,90 @@
+/*
+ ~ Author    : @tridib_2003
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll                  long long
+#define ull                 unsigned long long
+#define PI                  3.1415926535897932384626
+#define MOD                 1000000007
+#define vi                  vector<int>
+#define vvi                 vector<vector<int> >
+#define vll                 vector<long long>
+#define pb                  push_back
+#define eb                  emplace_back
+#define mp(a, b)            make_pair(a, b)
+#define pii                 pair<int, int>
+#define vpii                vector<pair<int, int> >
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(i, a, b)        for (int(i) = (a); (i) < (b); ++(i))
+#define RFOR(i, a, b)       for (int(i) = (a)-1; (i) >= (b); --(i))
+#define FORALL(i, a)         for (auto& (i) : (a))
+#define printall(a)         for (auto& (i) : (a)) cout << i << ' '
+#define rsort(a)            sort((a).rbegin(), (a).rend())
+#define w(x)                int x; cin >> x; while(x--)
+#define all(c)              (c).begin(), (c).end()
+#define sz(c)               (int)(c).size()
+#define pqmx                priority_queue<int>
+#define pqmn                priority_queue<int, vector<int>, greater<int> >
+#define max3(a, b, c)       max((a), max((b), (c)))
+#define min3(a, b, c)       min((a), min((b), (c)))
+#define mx_all(c)           *max_element((c).begin(), (c).end())
+#define mn_all(c)           *min_element((c).begin(), (c).end())
+#define lwr_b(c, a)         lower_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define upr_b(c, a)         upper_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+void solve() {
+
+	ll a, b, n;
+	cin >> a >> b >> n;
+
+	if (a % b == 0) {
+		cout << a;
+		FOR (i, 0, n) {
+			cout << 0;
+		}
+		return;
+	}
+
+	bool flag =  false;
+
+	FOR (digit, 0, 10) {
+
+		a = a * 10 + digit;
+
+		if (a % b == 0) {
+			flag = true;
+			break;
+		}
+
+		a /= 10;
+	}
+
+	if (!flag) {
+		cout << -1;
+		return;
+	}
+
+	cout << a;
+	FOR (i, 1, n) {
+		cout << 0;
+	}
+}
+
+int main() {
+
+	FIO;
+
+	int tc = 1;
+	// cin >> tc;
+	while (tc--) {
+		solve();
+		cout << '\n';
+	}
+
+	return 0;
+}
