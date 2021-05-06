cbb1d2cb70bdff73801d4892a1597596f790f9bd
Tridib Samanta <tridibsamanta2010@gmail.com>
2021-05-06 13:30:17 +0530

    315A. Sereja and Bottles

---
+++ 315A.cpp
@@ -0,0 +1,98 @@
+/*
+ ~ Author    : @tridib_2003
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll                  long long
+#define ull                 unsigned long long
+#define PI                  3.1415926535897932384626
+#define MOD                 1000000007
+#define vi                  vector<int>
+#define vvi                 vector<vector<int> >
+#define vll                 vector<long long>
+#define pb                  push_back
+#define eb                  emplace_back
+#define mp(a, b)            make_pair(a, b)
+#define pii                 pair<int, int>
+#define vpii                vector<pair<int, int> >
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(i, a, b)        for (int(i) = (a); (i) < (b); ++(i))
+#define RFOR(i, a, b)       for (int(i) = (a)-1; (i) >= (b); --(i))
+#define FORALL(i, a)         for (auto& (i) : (a))
+#define printall(a)         for (auto& (i) : (a)) cout << i << ' '
+#define rsort(a)            sort((a).rbegin(), (a).rend())
+#define w(x)                int x; cin >> x; while(x--)
+#define all(c)              (c).begin(), (c).end()
+#define sz(c)               (int)(c).size()
+#define pqmx                priority_queue<int>
+#define pqmn                priority_queue<int, vector<int>, greater<int> >
+#define max3(a, b, c)       max((a), max((b), (c)))
+#define min3(a, b, c)       min((a), min((b), (c)))
+#define mx_all(c)           *max_element((c).begin(), (c).end())
+#define mn_all(c)           *min_element((c).begin(), (c).end())
+#define lwr_b(c, a)         lower_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define upr_b(c, a)         upper_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+void solve() {
+
+	int n;
+	cin >> n;
+
+	vpii bottles;
+	vi freq(1001, 0);
+	vi sameCount(1001, 0);
+	vector<bool> allOpen(1001, false);
+
+	FOR (i, 0, n) {
+		int a, b;
+		cin >> a >> b;
+
+		bottles.eb(mp(a, b));
+
+		if (a == b)
+			++sameCount[b];
+
+		if (a != b)
+			allOpen[b] = true;
+
+		++freq[a];
+	}
+
+	FOR (i, 0, n) {
+		int a = bottles[i].first;
+		int b = bottles[i].second;
+
+		if (allOpen[b])
+			freq[b] = 0;
+		else if (sameCount[b] > 1)
+			freq[b] = 0;
+		else if (sameCount[b] == 1)
+			freq[b] = 1;
+	}
+
+	int cnt = 0;
+
+	FOR (i, 1, 1001) {
+		cnt += freq[i];
+	}
+
+	cout << cnt;
+}
+
+int main() {
+
+	FIO;
+
+	int tc = 1;
+	// cin >> tc;
+	while (tc--) {
+		solve();
+		cout << '\n';
+	}
+
+	return 0;
+}
