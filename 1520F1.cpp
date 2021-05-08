2cd80bd4aa384b086f66d1542932e0c56e7e30c4
Tridib Samanta <tridibsamanta2010@gmail.com>
2021-05-09 01:52:09 +0530

    1520F1. Guess the K-th Zero (Easy version)

---
+++ 1520F1.cpp
@@ -0,0 +1,84 @@
+/*
+ ~ Author    : @tridib_2003
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll                  long long
+#define ull                 unsigned long long
+#define PI                  3.1415926535897932384626
+#define MOD                 1000000007
+#define vi                  vector<int>
+#define vvi                 vector<vector<int> >
+#define vll                 vector<long long>
+#define pb                  push_back
+#define eb                  emplace_back
+#define mp(a, b)            make_pair(a, b)
+#define pii                 pair<int, int>
+#define vpii                vector<pair<int, int> >
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(i, a, b)        for (int(i) = (a); (i) < (b); ++(i))
+#define RFOR(i, a, b)       for (int(i) = (a)-1; (i) >= (b); --(i))
+#define FORALL(i, a)         for (auto& (i) : (a))
+#define printall(a)         for (auto& (i) : (a)) cout << i << ' '
+#define rsort(a)            sort((a).rbegin(), (a).rend())
+#define w(x)                int x; cin >> x; while(x--)
+#define all(c)              (c).begin(), (c).end()
+#define sz(c)               (int)(c).size()
+#define pqmx                priority_queue<int>
+#define pqmn                priority_queue<int, vector<int>, greater<int> >
+#define max3(a, b, c)       max((a), max((b), (c)))
+#define min3(a, b, c)       min((a), min((b), (c)))
+#define mx_all(c)           *max_element((c).begin(), (c).end())
+#define mn_all(c)           *min_element((c).begin(), (c).end())
+#define lwr_b(c, a)         lower_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define upr_b(c, a)         upper_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+int query(int l, int r) {
+	cout << "? " << l << ' ' << r << '\n';
+	cout.flush();
+	int sum;
+	cin >> sum;
+	return sum;
+}
+
+void solve() {
+
+	int n, t;
+	cin >> n >> t;
+
+	int k;
+	cin >> k;
+
+	int low = 0, high = n - 1;
+
+	while (low <= high) {
+
+		int mid = low + (high - low) / 2;
+		int currOnes = query(1, mid + 1), maxOnes = mid + 1;
+
+		if (currOnes > maxOnes - k)
+			low = mid + 1;
+		else
+			high = mid - 1;
+	}
+
+	cout << "! " << low + 1;
+}
+
+int main() {
+
+	FIO;
+
+	int tc = 1;
+	// cin >> tc;
+	while (tc--) {
+		solve();
+		cout << '\n';
+	}
+
+	return 0;
+}
