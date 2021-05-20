ac0d9b5574a6d6b98784556eaf1421f4d5a463bb
Tridib Samanta <tridibsamanta2010@gmail.com>
2021-05-20 20:38:28 +0530

    1341B. Nastya and Door

---
+++ 1341B.cpp
@@ -0,0 +1,98 @@
+/*
+ ~ Author    : @tridib_2003
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll                  long long
+#define ull                 unsigned long long
+#define PI                  3.1415926535897932384626
+#define MOD                 1000000007
+#define vi                  vector<int>
+#define vvi                 vector<vector<int> >
+#define vll                 vector<long long>
+#define pb                  push_back
+#define eb                  emplace_back
+#define mp(a, b)            make_pair(a, b)
+#define pii                 pair<int, int>
+#define vpii                vector<pair<int, int> >
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(i, a, b)        for (int(i) = (a); (i) < (b); ++(i))
+#define RFOR(i, a, b)       for (int(i) = (a)-1; (i) >= (b); --(i))
+#define FORALL(i, a)         for (auto& (i) : (a))
+#define printall(a)         for (auto& (i) : (a)) cout << i << ' '
+#define rsort(a)            sort((a).rbegin(), (a).rend())
+#define w(x)                int x; cin >> x; while(x--)
+#define all(c)              (c).begin(), (c).end()
+#define sz(c)               (int)(c).size()
+#define pqmx                priority_queue<int>
+#define pqmn                priority_queue<int, vector<int>, greater<int> >
+#define max3(a, b, c)       max((a), max((b), (c)))
+#define min3(a, b, c)       min((a), min((b), (c)))
+#define mx_all(c)           *max_element((c).begin(), (c).end())
+#define mn_all(c)           *min_element((c).begin(), (c).end())
+#define lwr_b(c, a)         lower_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define upr_b(c, a)         upper_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+const int MAXN = 1e5 + 5;
+
+int n, k;
+int height[2 * MAXN];
+int prefPeak[2 * MAXN];
+
+void computePrefPeak() {
+	prefPeak[1] = 0;
+
+	FOR (i, 2, n) {
+		if (height[i - 1] < height[i] && height[i] > height[i + 1])
+			prefPeak[i] = prefPeak[i - 1] + 1;
+		else
+			prefPeak[i] = prefPeak[i - 1];
+	}
+
+	prefPeak[n] = prefPeak[n - 1];
+}
+
+void solve() {
+
+	memset(height, 0, sizeof(height));
+	memset(prefPeak, 0, sizeof(prefPeak));
+
+	cin >> n >> k;
+
+	FOR (i, 1, n + 1) {
+		cin >> height[i];
+	}
+
+	computePrefPeak();
+
+	int maxPeak = INT_MIN, minLeft = 1;
+
+	FOR (i, k, n + 1) {
+		int l = i - k  + 1, r = i;
+		int peak = prefPeak[r - 1] - prefPeak[l];
+
+		if (peak > maxPeak) {
+			maxPeak = peak;
+			minLeft = l;
+		}
+	}
+
+	cout << maxPeak + 1 << ' ' << minLeft;
+}
+
+int32_t main() {
+
+	FIO;
+
+	int tc = 1;
+	cin >> tc;
+	while (tc--) {
+		solve();
+		cout << '\n';
+	}
+
+	return 0;
+}
