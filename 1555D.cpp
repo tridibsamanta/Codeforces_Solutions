1597f825162d6c13aea922edba269e5e35b6b2fc
Tridib Samanta <tridibsamanta2010@gmail.com>
2021-08-15 16:05:43 +0530

    1555D. Say No to Palindromes

---
+++ 1555D.cpp
@@ -0,0 +1,106 @@
+/*
+ ~ Author    : @tridib_2003
+*/
+
+#pragma GCC optimize("O3")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+        tree_order_statistics_node_update> indexed_set;
+
+#define ll                  long long
+#define ull                 unsigned long long
+#define PI                  3.1415926535897932384626
+#define MOD                 1000000007 // 998244353
+#define vi                  vector<int>
+#define vb                  vector<bool>
+#define vvi                 vector<vector<int> >
+#define vll                 vector<long long>
+#define pb                  push_back
+#define eb                  emplace_back
+#define mp(a, b)            make_pair(a, b)
+#define pii                 pair<int, int>
+#define vpii                vector<pair<int, int> >
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(i, a, b)        for (int(i) = (a); (i) < (b); ++(i))
+#define RFOR(i, a, b)       for (int(i) = (a)-1; (i) >= (b); --(i))
+#define FORALL(i, a)        for (auto& (i) : (a))
+#define printall(a)         for (auto& (i) : (a)) cout << i << ' '
+#define print(a)            cout << a << '\n'
+#define rsort(a)            sort((a).rbegin(), (a).rend())
+#define w(x)                int x; cin >> x; while(x--)
+#define all(c)              (c).begin(), (c).end()
+#define sz(c)               (int)(c).size()
+#define pqmx                priority_queue<int>
+#define pqmn                priority_queue<int, vector<int>, greater<int> >
+#define max3(a, b, c)       max((a), max((b), (c)))
+#define min3(a, b, c)       min((a), min((b), (c)))
+#define mx_all(c)           *max_element((c).begin(), (c).end())
+#define mn_all(c)           *min_element((c).begin(), (c).end())
+#define lwr_b(c, a)         lower_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define upr_b(c, a)         upper_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+const int MAXN = 2 * 1e5 + 5;
+
+static int dp[6][MAXN];
+// dp[i][j] = cost of the substring [1, j] for some i'th permutation of "abc"
+
+void solve() {
+
+	int n, m;
+	cin >> n >> m;
+
+	string s;
+	cin >> s;
+
+	memset(dp, 0, sizeof(dp));
+
+	string t = "abc"; // t = "abcabcabc..."
+	int perm_cnt = 0;
+
+	do {
+		FOR (i, 0, n) {
+			dp[perm_cnt][i + 1] = dp[perm_cnt][i] + (s[i] != t[i % 3]);
+		}
+
+		++perm_cnt;
+
+	} while (next_permutation(t.begin(), t.end()));
+
+	while (m--) {
+		int l, r;
+		cin >> l >> r;
+
+		int cost = n;
+
+		FOR (i, 0, 6) {
+			int currCost = dp[i][r] - dp[i][l - 1];
+			cost = min(cost, currCost);
+		}
+
+		cout << cost << '\n';
+	}
+}
+
+int32_t main() {
+
+	FIO;
+
+	int tc = 1;
+	// cin >> tc;
+	while (tc--) {
+		solve();
+		// cout << '\n';
+	}
+
+	return 0;
+}
