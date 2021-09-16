670327b515e7cc15d4cdd8db0302bd8c067730dd
Tridib Samanta <tridibsamanta2010@gmail.com>
2021-09-16 23:46:34 +0530

    96B. Lucky Numbers (easy)

---
+++ 96B.cpp
@@ -0,0 +1,94 @@
+/*
+ ~ Author    : @tridib_2003
+*/
+
+#pragma GCC optimize("O3")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+        tree_order_statistics_node_update> indexed_set;
+
+#define ll                  long long
+#define ull                 unsigned long long
+#define PI                  3.1415926535897932384626
+#define MOD                 998244353 // 1000000007
+#define vi                  vector<int>
+#define vb                  vector<bool>
+#define vvi                 vector<vector<int> >
+#define vll                 vector<long long>
+#define pb                  push_back
+#define eb                  emplace_back
+#define mp(a, b)            make_pair(a, b)
+#define pii                 pair<int, int>
+#define vpii                vector<pair<int, int> >
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(i, a, b)        for (int(i) = (a); (i) < (b); ++(i))
+#define RFOR(i, a, b)       for (int(i) = (a)-1; (i) >= (b); --(i))
+#define FORALL(i, a)        for (auto& (i) : (a))
+#define printall(a)         for (auto& (i) : (a)) cout << i << ' '
+#define print(a)            cout << a << '\n'
+#define rsort(a)            sort((a).rbegin(), (a).rend())
+#define w(x)                int x; cin >> x; while(x--)
+#define all(c)              (c).begin(), (c).end()
+#define sz(c)               (int)(c).size()
+#define pqmx                priority_queue<int>
+#define pqmn                priority_queue<int, vector<int>, greater<int> >
+#define max3(a, b, c)       max((a), max((b), (c)))
+#define min3(a, b, c)       min((a), min((b), (c)))
+#define mx_all(c)           *max_element((c).begin(), (c).end())
+#define mn_all(c)           *min_element((c).begin(), (c).end())
+#define lwr_b(c, a)         lower_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define upr_b(c, a)         upper_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+vll superLucky;
+
+void generateSuperLucky(ll n, ll four_cnt, ll seven_cnt) {
+	if (n > 1e10)
+		return;
+
+	if (four_cnt == seven_cnt)
+		superLucky.eb(n);
+
+	ll add_four = n * 10 + 4;
+	ll add_seven = n * 10 + 7;
+
+	generateSuperLucky(add_four, four_cnt + 1, seven_cnt);
+	generateSuperLucky(add_seven, four_cnt, seven_cnt + 1);
+}
+
+void solve() {
+
+	ll n;
+	cin >> n;
+
+	generateSuperLucky(4, 1, 0);
+	generateSuperLucky(7, 0, 1);
+
+	sort(all(superLucky));
+
+	int idx = lwr_b(superLucky, n);
+	cout << superLucky[idx];
+}
+
+int32_t main() {
+
+	FIO;
+
+	int tc = 1;
+	// cin >> tc;
+	while (tc--) {
+		solve();
+		// cout << '\n';
+	}
+
+	return 0;
+}
