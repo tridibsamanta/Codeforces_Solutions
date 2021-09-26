089ec4f23b429e4bedf1d35c7fea266773e52726
Tridib Samanta <tridibsamanta2010@gmail.com>
2021-09-26 20:13:47 +0530

    279A. Point on Spiral

---
+++ 279A.cpp
@@ -0,0 +1,120 @@
+/*
+ ~ Author    : @tridib_2003
+*/
+
+#pragma GCC optimize("O3")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+        tree_order_statistics_node_update> indexed_set;
+
+#define ll                  long long
+#define ull                 unsigned long long
+#define PI                  3.1415926535897932384626
+#define MOD                 1000000007 // 998244353
+#define vi                  vector<int>
+#define vb                  vector<bool>
+#define vvi                 vector<vector<int> >
+#define vll                 vector<long long>
+#define pb                  push_back
+#define eb                  emplace_back
+#define mp(a, b)            make_pair(a, b)
+#define pii                 pair<int, int>
+#define vpii                vector<pair<int, int> >
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(i, a, b)        for (int(i) = (a); (i) < (b); ++(i))
+#define RFOR(i, a, b)       for (int(i) = (a)-1; (i) >= (b); --(i))
+#define FORALL(i, a)        for (auto& (i) : (a))
+#define printall(a)         for (auto& (i) : (a)) cout << i << ' '
+#define print(a)            cout << a << '\n'
+#define rsort(a)            sort((a).rbegin(), (a).rend())
+#define w(x)                int x; cin >> x; while(x--)
+#define all(c)              (c).begin(), (c).end()
+#define sz(c)               (int)(c).size()
+#define pqmx                priority_queue<int>
+#define pqmn                priority_queue<int, vector<int>, greater<int> >
+#define max3(a, b, c)       max((a), max((b), (c)))
+#define min3(a, b, c)       min((a), min((b), (c)))
+#define mx_all(c)           *max_element((c).begin(), (c).end())
+#define mn_all(c)           *min_element((c).begin(), (c).end())
+#define lwr_b(c, a)         lower_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define upr_b(c, a)         upper_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+void solve() {
+
+	int x, y;
+	cin >> x >> y;
+
+	if (x == 0 && y == 0) {
+		cout << 0;
+		return;
+	}
+
+	int turns = -1;
+	int curr_x = 0, curr_y = 0, prev_x = 0, prev_y = 0;
+
+	while (true) {
+		// move right
+		++turns;
+		curr_x *= -1;
+		++curr_x;
+
+		if ((x > prev_x && x <= curr_x) && (y == prev_y))
+			break;
+
+		prev_x = curr_x;
+
+		// move up
+		++turns;
+		curr_y *= -1;
+		++curr_y;
+
+		if ((y > prev_y && y <= curr_y) && (x == prev_x))
+			break;
+
+		prev_y = curr_y;
+
+		// move left
+		++turns;
+		curr_x *= -1;
+
+		if ((x >= curr_x && x < prev_x) && (y == prev_y))
+			break;
+
+		prev_x = curr_x;
+
+		// move down
+		++turns;
+		curr_y *= -1;
+
+		if ((y >= curr_y && y < prev_y) && (x == prev_x))
+			break;
+
+		prev_y = curr_y;
+	}
+
+	cout << turns;
+}
+
+int32_t main() {
+
+	FIO;
+
+	int tc = 1;
+	// cin >> tc;
+	while (tc--) {
+		solve();
+		cout << '\n';
+	}
+
+	return 0;
+}
