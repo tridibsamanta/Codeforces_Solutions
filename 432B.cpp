b4c1d802b9e3f8710de45c01ba3636ad169b4205
Tridib Samanta <tridibsamanta2010@gmail.com>
2021-10-05 02:38:49 +0530

    432B. Football Kit

---
+++ 432B.cpp
@@ -0,0 +1,99 @@
+/*
+ ~ Author    : @tridib_2003
+*/
+
+#pragma GCC optimize("O3")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+        tree_order_statistics_node_update> indexed_set;
+
+#define ll                  long long
+#define ull                 unsigned long long
+#define PI                  3.1415926535897932384626
+#define MOD                 1000000007 // 998244353
+#define vi                  vector<int>
+#define vb                  vector<bool>
+#define vvi                 vector<vector<int> >
+#define vll                 vector<long long>
+#define pb                  push_back
+#define eb                  emplace_back
+#define mp(a, b)            make_pair(a, b)
+#define pii                 pair<int, int>
+#define vpii                vector<pair<int, int> >
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(i, a, b)        for (int(i) = (a); (i) < (b); ++(i))
+#define RFOR(i, a, b)       for (int(i) = (a)-1; (i) >= (b); --(i))
+#define FORALL(i, a)        for (auto& (i) : (a))
+#define printall(a)         for (auto& (i) : (a)) cout << i << ' '
+#define print(a)            cout << a << '\n'
+#define rsort(a)            sort((a).rbegin(), (a).rend())
+#define w(x)                int x; cin >> x; while(x--)
+#define all(c)              (c).begin(), (c).end()
+#define sz(c)               (int)(c).size()
+#define pqmx                priority_queue<int>
+#define pqmn                priority_queue<int, vector<int>, greater<int> >
+#define max3(a, b, c)       max((a), max((b), (c)))
+#define min3(a, b, c)       min((a), min((b), (c)))
+#define mx_all(c)           *max_element((c).begin(), (c).end())
+#define mn_all(c)           *min_element((c).begin(), (c).end())
+#define lwr_b(c, a)         lower_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define upr_b(c, a)         upper_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+const int MAXN = 1e5 + 5;
+
+void solve() {
+
+	int n;
+	cin >> n;
+
+	vvi kitColor(n, vi (2));
+	vvi homeKit(MAXN);
+	vvi awayKit(MAXN);
+
+	FOR (i, 0, n) {
+		cin >> kitColor[i][0] >> kitColor[i][1];
+		homeKit[kitColor[i][0]].eb(i);
+		awayKit[kitColor[i][1]].eb(i);
+	}
+
+	vvi matchKitColor(n, vi {n - 1, n - 1});
+
+	FOR (i, 1, MAXN) {
+		int cnt = sz(homeKit[i]);
+
+		if (cnt > 0 && sz(awayKit[i])) {
+			FOR (j, 0, sz(awayKit[i])) {
+				matchKitColor[awayKit[i][j]][0] += cnt;
+				matchKitColor[awayKit[i][j]][1] -= cnt;
+			}
+		}
+	}
+
+	FOR (i, 0, n) {
+		cout << matchKitColor[i][0] << ' ' << matchKitColor[i][1] << '\n';
+	}
+}
+
+int32_t main() {
+
+	FIO;
+
+	int tc = 1;
+	// cin >> tc;
+	while (tc--) {
+		solve();
+		cout << '\n';
+	}
+
+	return 0;
+}
