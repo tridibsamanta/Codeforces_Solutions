4e3976777d92578edb028b3555755270774ec80a
Tridib Samanta <tridibsamanta2010@gmail.com>
2021-11-09 22:48:48 +0530

    1529D. Kavi on Pairing Duty

---
+++ 1529D.cpp
@@ -0,0 +1,101 @@
+/*
+ ~ Author    : @tridib_2003
+*/
+
+#pragma GCC optimize("O3")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+        tree_order_statistics_node_update> indexed_set;
+
+#define int                 long long
+#define ull                 unsigned long long
+#define PI                  3.1415926535897932384626
+#define MOD                 998244353 // 1000000007
+#define vi                  vector<int>
+#define vb                  vector<bool>
+#define vvi                 vector<vector<int> >
+#define vll                 vector<long long>
+#define pb                  push_back
+#define eb                  emplace_back
+#define mp(a, b)            make_pair(a, b)
+#define pii                 pair<int, int>
+#define vpii                vector<pair<int, int> >
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(i, a, b)        for (int(i) = (a); (i) < (b); ++(i))
+#define RFOR(i, a, b)       for (int(i) = (a)-1; (i) >= (b); --(i))
+#define FORALL(i, a)        for (auto& (i) : (a))
+#define printall(a)         for (auto& (i) : (a)) cout << i << ' '
+#define print(a)            cout << a << '\n'
+#define rsort(a)            sort((a).rbegin(), (a).rend())
+#define w(x)                int x; cin >> x; while(x--)
+#define all(c)              (c).begin(), (c).end()
+#define sz(c)               (int)(c).size()
+#define pqmx                priority_queue<int>
+#define pqmn                priority_queue<int, vector<int>, greater<int> >
+#define max3(a, b, c)       max((a), max((b), (c)))
+#define min3(a, b, c)       min((a), min((b), (c)))
+#define mx_all(c)           *max_element((c).begin(), (c).end())
+#define mn_all(c)           *min_element((c).begin(), (c).end())
+#define lwr_b(c, a)         lower_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define upr_b(c, a)         upper_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+int n;
+vi factorCnt;
+vi dp, prefDp;
+
+void getFactors() {
+	factorCnt.clear();
+	factorCnt.resize(n + 1);
+
+	for (int i = 1; i <= n; ++i) {
+		for (int j = i; j <= n; j += i) {
+			++factorCnt[j];
+		}
+	}
+}
+
+void solve() {
+
+	cin >> n;
+
+	getFactors();
+
+	dp.clear();
+	dp.resize(2 * n + 1, 0);
+	dp[0] = 1;
+
+	prefDp.clear();
+	prefDp.resize(2 * n + 1, 0);
+	prefDp[0] = 1;
+
+	for (int i = 2; i <= 2 * n; i += 2) {
+		dp[i] = (prefDp[i - 2] + factorCnt[i / 2] - 1) % MOD;
+		prefDp[i] = (prefDp[i - 2] + dp[i]) % MOD;
+	}
+
+	cout << dp[2 * n];
+}
+
+int32_t main() {
+
+	FIO;
+
+	int tc = 1;
+	// cin >> tc;
+	while (tc--) {
+		solve();
+		cout << '\n';
+	}
+
+	return 0;
+}
