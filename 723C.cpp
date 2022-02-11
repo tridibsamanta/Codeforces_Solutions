1c48adc4933655d73aff96d7e9195f346465aec4
Tridib Samanta <tridibsamanta2010@gmail.com>
2022-02-12 03:31:55 +0530

    723C. Polycarp at the Radio

---
+++ 723C.cpp
@@ -0,0 +1,111 @@
+/*
+ ~ Author    : @tridib_2003
+*/
+
+#pragma GCC optimize("O3")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+        tree_order_statistics_node_update> indexed_set;
+
+#define int                 long long
+#define ull                 unsigned long long
+#define PI                  3.1415926535897932384626
+#define MOD                 1000000007 // 998244353
+#define vi                  vector<int>
+#define vb                  vector<bool>
+#define vvi                 vector<vector<int> >
+#define vll                 vector<long long>
+#define pb                  push_back
+#define eb                  emplace_back
+#define mp(a, b)            make_pair(a, b)
+#define pii                 pair<int, int>
+#define vpii                vector<pair<int, int> >
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(i, a, b)        for (int(i) = (a); (i) < (b); ++(i))
+#define RFOR(i, a, b)       for (int(i) = (a)-1; (i) >= (b); --(i))
+#define FORALL(i, a)        for (auto& (i) : (a))
+#define printall(a)         for (auto& (i) : (a)) cout << i << ' '
+#define print(a)            cout << a << '\n'
+#define rsort(a)            sort((a).rbegin(), (a).rend())
+#define w(x)                int x; cin >> x; while(x--)
+#define all(c)              (c).begin(), (c).end()
+#define sz(c)               (int)(c).size()
+#define pqmx                priority_queue<int>
+#define pqmn                priority_queue<int, vector<int>, greater<int> >
+#define max3(a, b, c)       max((a), max((b), (c)))
+#define min3(a, b, c)       min((a), min((b), (c)))
+#define mx_all(c)           *max_element((c).begin(), (c).end())
+#define mn_all(c)           *min_element((c).begin(), (c).end())
+#define lwr_b(c, a)         lower_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define upr_b(c, a)         upper_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+void solve() {
+	int n, m;
+	cin >> n >> m;
+
+	bool isDivisible = (n % m == 0);
+	int lead_cnt = n % m;
+	int min_cnt = n / m;
+
+	vi arr(n);
+	vi replace_idx;
+	vi freq(2001, 0);
+
+	FOR (i, 0, n) {
+		cin >> arr[i];
+
+		if (arr[i] > m)
+			replace_idx.eb(i);
+		else if (++freq[arr[i]] > min_cnt) {
+			if (lead_cnt && freq[arr[i]] == min_cnt + 1)
+				--lead_cnt;
+			else
+				replace_idx.eb(i);
+		}
+	}
+
+	int change_cnt = 0;
+
+	FOR (i, 1, m + 1) {
+		int curr_change = min_cnt - freq[i];
+
+		if (curr_change > 0) {
+			change_cnt += curr_change;
+
+			while (curr_change--) {
+				int idx = replace_idx.back();
+				replace_idx.pop_back();
+
+				arr[idx] = i;
+			}
+		}
+	}
+
+	cout << min_cnt << ' ' << change_cnt << '\n';
+
+	printall(arr);
+}
+
+int32_t main() {
+
+	FIO;
+
+	int tc = 1;
+	// cin >> tc;
+	while (tc--) {
+		solve();
+		cout << '\n';
+	}
+
+	return 0;
+}
