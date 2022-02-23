984e617d6b52dcfb7b29218595e0af27d789d0d5
Tridib Samanta <tridibsamanta2010@gmail.com>
2022-02-23 21:28:55 +0530

    1360D. Buying Shovels

---
+++ 1360D.cpp
@@ -0,0 +1,108 @@
+/*
+ ~ Author    : @tridib_2003
+*/
+
+#pragma GCC optimize("O3")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+        tree_order_statistics_node_update> indexed_set;
+
+#define int                 long long
+#define ull                 unsigned long long
+#define PI                  3.1415926535897932384626
+#define MOD                 1000000007 // 998244353
+#define vi                  vector<int>
+#define vb                  vector<bool>
+#define vvi                 vector<vector<int> >
+#define vll                 vector<long long>
+#define pb                  push_back
+#define eb                  emplace_back
+#define mp(a, b)            make_pair(a, b)
+#define pii                 pair<int, int>
+#define vpii                vector<pair<int, int> >
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(i, a, b)        for (int(i) = (a); (i) < (b); ++(i))
+#define RFOR(i, a, b)       for (int(i) = (a)-1; (i) >= (b); --(i))
+#define FORALL(i, a)        for (auto& (i) : (a))
+#define printall(a)         for (auto& (i) : (a)) cout << i << ' '
+#define print(a)            cout << a << '\n'
+#define rsort(a)            sort((a).rbegin(), (a).rend())
+#define w(x)                int x; cin >> x; while(x--)
+#define all(c)              (c).begin(), (c).end()
+#define sz(c)               (int)(c).size()
+#define pqmx                priority_queue<int>
+#define pqmn                priority_queue<int, vector<int>, greater<int> >
+#define max3(a, b, c)       max((a), max((b), (c)))
+#define min3(a, b, c)       min((a), min((b), (c)))
+#define mx_all(c)           *max_element((c).begin(), (c).end())
+#define mn_all(c)           *min_element((c).begin(), (c).end())
+#define lwr_b(c, a)         lower_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define upr_b(c, a)         upper_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+vector<int> factors;
+
+void factorize(int n) {
+	for (int i = 1; i * i <= n; ++i) {
+		if (n % i == 0) {
+			factors.emplace_back(i);
+
+			if (i != (n / i))
+				factors.emplace_back(n / i);
+		}
+	}
+}
+
+void solve() {
+	int n, k;
+	cin >> n >> k;
+
+	if (n <= k) {
+		cout << 1;
+		return;
+	}
+
+	if (k == 1) {
+		cout << n;
+		return;
+	}
+
+	factors.clear();
+
+	factorize(n);
+
+	rsort(factors);
+
+	FOR (i, 0, sz(factors)) {
+		if (factors[i] > k)
+			continue;
+
+		if (n % factors[i] == 0) {
+			cout << n / factors[i];
+			return;
+		}
+	}
+}
+
+int32_t main() {
+
+	FIO;
+
+	int tc = 1;
+	cin >> tc;
+	while (tc--) {
+		solve();
+		cout << '\n';
+	}
+
+	return 0;
+}
