f7b0154050515c00008249e03e20704a2a9d7652
Tridib Samanta <tridibsamanta2010@gmail.com>
2022-02-26 14:27:36 +0530

    1644D. Cross Coloring

---
+++ 1644D.cpp
@@ -0,0 +1,103 @@
+/*
+ ~ Author    : @tridib_2003
+*/
+
+#pragma GCC optimize("O3")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+        tree_order_statistics_node_update> indexed_set;
+
+#define int                 long long
+#define ull                 unsigned long long
+#define PI                  3.1415926535897932384626
+#define MOD                 998244353 // 1000000007
+#define vi                  vector<int>
+#define vb                  vector<bool>
+#define vvi                 vector<vector<int> >
+#define vll                 vector<long long>
+#define pb                  push_back
+#define eb                  emplace_back
+#define mp(a, b)            make_pair(a, b)
+#define pii                 pair<int, int>
+#define vpii                vector<pair<int, int> >
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(i, a, b)        for (int(i) = (a); (i) < (b); ++(i))
+#define RFOR(i, a, b)       for (int(i) = (a)-1; (i) >= (b); --(i))
+#define FORALL(i, a)        for (auto& (i) : (a))
+#define printall(a)         for (auto& (i) : (a)) cout << i << ' '
+#define print(a)            cout << a << '\n'
+#define rsort(a)            sort((a).rbegin(), (a).rend())
+#define w(x)                int x; cin >> x; while(x--)
+#define all(c)              (c).begin(), (c).end()
+#define sz(c)               (int)(c).size()
+#define pqmx                priority_queue<int>
+#define pqmn                priority_queue<int, vector<int>, greater<int> >
+#define max3(a, b, c)       max((a), max((b), (c)))
+#define min3(a, b, c)       min((a), min((b), (c)))
+#define mx_all(c)           *max_element((c).begin(), (c).end())
+#define mn_all(c)           *min_element((c).begin(), (c).end())
+#define lwr_b(c, a)         lower_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define upr_b(c, a)         upper_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+void solve() {
+	int n, m, k, q;
+	cin >> n >> m >> k >> q;
+
+	vpii arr;
+
+	FOR (i, 0, q) {
+		int x, y;
+		cin >> x >> y;
+
+		arr.pb({x, y});
+	}
+
+	set<int> row, col;
+	int ans = 1;
+
+	RFOR (i, q, 0) {
+		int r = arr[i].first;
+		int c = arr[i].second;
+
+		bool is_row_colored = false, is_col_colored = false;
+
+		if (sz(col) == m || row.count(r))
+			is_row_colored = true;
+
+		if (sz(row) == n || col.count(c))
+			is_col_colored = true;
+
+		if (!(is_row_colored && is_col_colored)) {
+			row.insert(r);
+			col.insert(c);
+
+			ans = (ans * k) % MOD;
+		}
+	}
+
+	cout << ans;
+}
+
+int32_t main() {
+
+	FIO;
+
+	int tc = 1;
+	cin >> tc;
+	while (tc--) {
+		solve();
+		cout << '\n';
+	}
+
+	return 0;
+}
