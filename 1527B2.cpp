c41eccbbf96ad540a08ccf624495f9feeeb35221
Tridib Samanta <tridibsamanta2010@gmail.com>
2022-03-09 23:37:55 +0530

    1527B2. Palindrome Game (hard version)

---
+++ 1527B2.cpp
@@ -0,0 +1,100 @@
+/*
+ ~ Author    : @tridib_2003
+*/
+
+#pragma GCC optimize("O3")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+        tree_order_statistics_node_update> indexed_set;
+
+#define int                 long long
+#define ull                 unsigned long long
+#define PI                  3.1415926535897932384626
+#define MOD                 1000000007 // 998244353 
+#define vi                  vector<int>
+#define vb                  vector<bool>
+#define vvi                 vector<vector<int> >
+#define vll                 vector<long long>
+#define pb                  push_back
+#define eb                  emplace_back
+#define mp(a, b)            make_pair(a, b)
+#define pii                 pair<int, int>
+#define vpii                vector<pair<int, int> >
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(i, a, b)        for (int(i) = (a); (i) < (b); ++(i))
+#define RFOR(i, a, b)       for (int(i) = (a)-1; (i) >= (b); --(i))
+#define FORALL(i, a)        for (auto& (i) : (a))
+#define printall(a)         for (auto& (i) : (a)) cout << i << ' '
+#define print(a)            cout << a << '\n'
+#define rsort(a)            sort((a).rbegin(), (a).rend())
+#define w(x)                int x; cin >> x; while(x--)
+#define all(c)              (c).begin(), (c).end()
+#define sz(c)               (int)(c).size()
+#define pqmx                priority_queue<int>
+#define pqmn                priority_queue<int, vector<int>, greater<int> >
+#define max3(a, b, c)       max((a), max((b), (c)))
+#define min3(a, b, c)       min((a), min((b), (c)))
+#define mx_all(c)           *max_element((c).begin(), (c).end())
+#define mn_all(c)           *min_element((c).begin(), (c).end())
+#define lwr_b(c, a)         lower_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define upr_b(c, a)         upper_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+void solve() {
+	int n;
+	cin >> n;
+
+	string s;
+	cin >> s;
+
+	int zero_cnt = 0, one_cnt = 0;
+
+	for (int i = 0; i < n; ++i) {
+		if (s[i] == '0')
+			++zero_cnt;
+	}
+
+	for (int i = 0; i < n / 2; ++i) {
+		if ((s[i] == '1' || s[n - 1 - i] == '1') && s[i] != s[n - 1 - i])
+			++one_cnt;
+	}
+
+	string rev_s = s;
+	reverse(all(rev_s));
+
+	if (s == rev_s) {
+		if (zero_cnt == 1)
+			cout << "BOB";
+		else if (zero_cnt & 1)
+			cout << "ALICE";
+		else
+			cout << "BOB";
+
+		return;
+	}
+
+	cout << ((one_cnt == 1 && zero_cnt == 2) ? "DRAW" : "ALICE");
+}
+
+int32_t main() {
+
+	FIO;
+
+	int tc = 1;
+	cin >> tc;
+	while (tc--) {
+		solve();
+		cout << '\n';
+	}
+
+	return 0;
+}
