54a06929b34d9cbdd3f1ba32bed72c02d0657d20
Tridib Samanta <tridibsamanta2010@gmail.com>
2022-04-03 13:41:00 +0530

    1651C. Fault-tolerant Network

---
+++ 1651C.cpp
@@ -0,0 +1,106 @@
+/*
+ ~ Author    : @tridib_2003
+*/
+
+#pragma GCC optimize("O3")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+        tree_order_statistics_node_update> indexed_set;
+
+#define int                 long long
+#define ull                 unsigned long long
+#define PI                  3.1415926535897932384626
+#define MOD                 998244353 // 1000000007
+#define vi                  vector<int>
+#define vb                  vector<bool>
+#define vvi                 vector<vector<int> >
+#define vll                 vector<long long>
+#define pb                  push_back
+#define eb                  emplace_back
+#define mp(a, b)            make_pair(a, b)
+#define pii                 pair<int, int>
+#define vpii                vector<pair<int, int> >
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(i, a, b)        for (int(i) = (a); (i) < (b); ++(i))
+#define RFOR(i, a, b)       for (int(i) = (a)-1; (i) >= (b); --(i))
+#define FORALL(i, a)        for (auto& (i) : (a))
+#define printall(a)         for (auto& (i) : (a)) cout << i << ' '
+#define print(a)            cout << a << '\n'
+#define rsort(a)            sort((a).rbegin(), (a).rend())
+#define w(x)                int x; cin >> x; while(x--)
+#define all(c)              (c).begin(), (c).end()
+#define sz(c)               (int)(c).size()
+#define pqmx                priority_queue<int>
+#define pqmn                priority_queue<int, vector<int>, greater<int> >
+#define max3(a, b, c)       max((a), max((b), (c)))
+#define min3(a, b, c)       min((a), min((b), (c)))
+#define mx_all(c)           *max_element((c).begin(), (c).end())
+#define mn_all(c)           *min_element((c).begin(), (c).end())
+#define lwr_b(c, a)         lower_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define upr_b(c, a)         upper_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+const int INF = 1e9;
+
+void solve() {
+	int n;
+	cin >> n;
+
+	vi arr1(n);
+
+	FOR (i, 0, n) {
+		cin >> arr1[i];
+	}
+
+	vi arr2(n);
+
+	FOR (i, 0, n) {
+		cin >> arr2[i];
+	}
+
+	int diff_A1 = INF, diff_An = INF;
+	int diff_B1 = INF, diff_Bn = INF;
+
+	FOR (i, 0, n) {
+		diff_A1 = min(diff_A1, abs(arr1[0] - arr2[i]));
+		diff_An = min(diff_An, abs(arr1[n - 1] - arr2[i]));
+		diff_B1 = min(diff_B1, abs(arr2[0] - arr1[i]));
+		diff_Bn = min(diff_Bn, abs(arr2[n - 1] - arr1[i]));
+	}
+
+	int ans = diff_A1 + diff_An + diff_B1 + diff_Bn;
+
+	ans = min(ans, abs(arr1[0] - arr2[0]) + diff_An + diff_Bn);
+	ans = min(ans, abs(arr1[0] - arr2[n - 1]) + diff_An + diff_B1);
+
+	ans = min(ans, abs(arr1[n - 1] - arr2[0]) + diff_A1 + diff_Bn);
+	ans = min(ans, abs(arr1[n - 1] - arr2[n - 1]) + diff_A1 + diff_B1);
+
+	ans = min(ans, abs(arr1[0] - arr2[0]) + abs(arr1[n - 1] - arr2[n - 1]));
+	ans = min(ans, abs(arr1[0] - arr2[n - 1]) + abs(arr1[n - 1] - arr2[0]));
+
+	cout << ans;
+}
+
+int32_t main() {
+
+	FIO;
+
+	int tc = 1;
+	cin >> tc;
+	while (tc--) {
+		solve();
+		cout << '\n';
+	}
+
+	return 0;
+}
