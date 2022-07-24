112b37ffa26fba6dfcc67ec073b37002b4f345a7
Tridib Samanta <tridibsamanta2010@gmail.com>
2022-07-24 23:46:17 +0530

    217A. Ice Skating

---
+++ 217A.cpp
@@ -0,0 +1,118 @@
+/*
+ ~ Author    : @tridib_2003
+*/
+
+#pragma GCC optimize("O3")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+        tree_order_statistics_node_update> indexed_set;
+
+#define int                 long long
+#define ull                 unsigned long long
+#define PI                  3.1415926535897932384626
+#define MOD                 998244353 // 1000000007
+#define vi                  vector<int>
+#define vb                  vector<bool>
+#define vvi                 vector<vector<int> >
+#define vll                 vector<long long>
+#define pb                  push_back
+#define eb                  emplace_back
+#define mp(a, b)            make_pair(a, b)
+#define pii                 pair<int, int>
+#define vpii                vector<pair<int, int> >
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(i, a, b)        for (int(i) = (a); (i) < (b); ++(i))
+#define RFOR(i, a, b)       for (int(i) = (a)-1; (i) >= (b); --(i))
+#define FORALL(i, a)        for (auto& (i) : (a))
+#define printall(a)         for (auto& (i) : (a)) cout << i << ' '
+#define print(a)            cout << a << '\n'
+#define rsort(a)            sort((a).rbegin(), (a).rend())
+#define w(x)                int x; cin >> x; while(x--)
+#define all(c)              (c).begin(), (c).end()
+#define sz(c)               (int)(c).size()
+#define pqmx                priority_queue<int>
+#define pqmn                priority_queue<int, vector<int>, greater<int> >
+#define max3(a, b, c)       max((a), max((b), (c)))
+#define min3(a, b, c)       min((a), min((b), (c)))
+#define mx_all(c)           *max_element((c).begin(), (c).end())
+#define mn_all(c)           *min_element((c).begin(), (c).end())
+#define lwr_b(c, a)         lower_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define upr_b(c, a)         upper_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+struct DSU {
+	int n, comp;
+	vector<int> parent, compSz;
+	void init(int N) {
+		n = N;
+		parent.resize(n + 1, -1);
+		compSz.resize(n + 1, 1);
+		comp = n;
+	}
+	int Find(int x) {
+		if (parent[x] < 0)
+			return x;
+		return parent[x] = Find(parent[x]);
+	}
+	bool Merge(int x, int y) {
+		x = Find(x);
+		y = Find(y);
+		if (x == y)
+			return false;
+		if (compSz[x] > compSz[y])
+			swap(x, y);
+		parent[x] = y;
+		compSz[y] += compSz[x];
+		--comp;
+		return true;
+	}
+};
+
+void solve() {
+	int n;
+	cin >> n;
+
+	vpii points(n);
+
+	FOR (i, 0, n) {
+		cin >> points[i].first >> points[i].second;
+	}
+
+	DSU d1;
+	d1.init(n);
+
+	FOR (i, 0, n) {
+		FOR (j, i + 1, n) {
+			if (points[i].first == points[j].first)
+				d1.Merge(i + 1, j + 1);
+
+			if (points[i].second == points[j].second)
+				d1.Merge(i + 1, j + 1);
+		}
+	}
+
+	cout << (d1.comp - 1);
+}
+
+int32_t main() {
+
+	FIO;
+
+	int tc = 1;
+	// cin >> tc;
+	while (tc--) {
+		solve();
+		cout << '\n';
+	}
+
+	return 0;
+}
